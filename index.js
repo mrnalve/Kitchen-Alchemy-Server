2756d5bd635bcdd700b7606705766d17d2bc33a9
Md Alve <mdalve1091@gmail.com>
2023-05-02 12:12:15 +0600

    added banner image in json file

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ const chefsInfo = require('./data/chefInfo.json')
 app.get('/chefs', (req,res)=>{
     res.send(chefsInfo)
 })
+app.get('/chefs/:id',(req, res)=>{
+    const id = req.params.id
+    const selectedChef = chefsInfo.find(chef => chef.id == id)
+    res.send(selectedChef)
+})
 
 app.listen(port, ()=>{
     console.log('dragon api running');
 })
 
-/*
-<img src="https://i.ibb.co/dgLnD5T/Jo-l-Robuchon.jpg" alt="Jo-l-Robuchon" border="0">
-<img src="https://i.ibb.co/NxCKYDn/Yannick-All-no.jpg" alt="Yannick-All-no" border="0">
-<img src="https://i.ibb.co/X3WYWKk/Alain-Passard.jpg" alt="Alain-Passard" border="0">
-<img src="https://i.ibb.co/LndyrQL/Anne-Sophie-Pic.jpg" alt="Anne-Sophie-Pic" border="0">
-<img src="https://i.ibb.co/VNXLzjB/Guy-Savoy.jpg" alt="Guy-Savoy" border="0">
-<img src="https://i.ibb.co/M5JTd69/H-l-ne-Darroze.jpg" alt="H-l-ne-Darroze" border="0">
-*/ 
